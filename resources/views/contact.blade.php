efe29fe52b70296d8b19d6547f3cfbeb3f97e45f
Heng David <hengdavid129@gmail.com>
2026-03-31 00:57:55 +0700

    Layoyt Files

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Contact Page</h1>
+<x-layout title="Contact Us">
+    <h1>Contact Us</h1>
 
-    <div>
-        <a href="/">Return Home</a>
-    </div>
-</body>
-</html>
+    <x-card class="max-w-400">
+        <p>Placeholder for the contact form.</p>
+    </x-card>
+</x-layout>
