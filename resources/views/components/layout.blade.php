4aaa4d6fe42f75a7116d59ca9456ae5a0f0846dd
Heng David <hengdavid129@gmail.com>
2026-04-06 15:30:03 +0700

    Forms

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     <style>
         .max-w-400 {
             max-width: 400px;
@@ -21,13 +22,7 @@
         }
     </style>
 </head>
-<body>
-    <nav>
-        <a href="/">Home</a>
-        <a href="/about">About Us</a>
-        <a href="/contact">Contact Us</a>
-    </nav>
-
+<body class="bg-gray-700 p-6 max-w-xl mx-auto">
     <main>
         {{ $slot }}
     </main>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,32 @@
 <?php
 
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function() {
-    return view('welcome', [
-        'tasks' => []
+Route::get('/', function () {
+    $ideas = session()->get('ideas', []);
+    return view('ideas', [
+        'ideas' => $ideas
     ]);
+
 });
 
-Route::view('/about', 'about');
-Route::view('/contact', 'contact');
+Route::post('/ideas', function () {
+    // $idea = Request::input('idea');
+    $idea = request('idea');
+
+    session()->push('ideas', $idea);
+
+    return redirect('/');
+});
+
+// Route::post('/ideas', function (Request $request) {
+//     dd($request->idea);
+// });
+
+Route::get('/delete-ideas', function () {
+    session()->forget('ideas');
+
+    return redirect('/');
+});
