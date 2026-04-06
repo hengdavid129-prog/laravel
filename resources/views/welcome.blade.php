242de9a147a1fabc3632dbe9dddd46787c3e663b
Heng David <hengdavid129@gmail.com>
2026-04-06 11:54:34 +0700

    Blade Directives

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,12 @@
 <x-layout title="Home">
-    <p>
-        {{ $greeting }}, {{ $person }}!
-    </p>
+
+    @can('edit', $post)
+        <a href="/posts/1/edit">Edit</a>
+    @endcan
+    
+    {{-- @forelse ($tasks as $tak)
+        <li>{{ $task }}</li>
+    @empty
+        <p>There are no active tasks.</p>
+    @endforelse --}}
 </x-layout>
