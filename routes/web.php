7a37ff0e51d93eeec27313169a0de75f00ac8825
Heng David <hengdavid129@gmail.com>
2026-04-08 10:33:47 +0700

    HTTP Requests and REST

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,47 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Idea;
 
-Route::get('/', function () {
-    $ideas = Idea::query()
-        ->when(request('state'), function($query, $state) {
-            $query->where('state', $state);
-        })
-        ->get();
+// index
+Route::get('/ideas', function () {
+    $ideas = Idea::all();
 
-    return view('ideas', [
+    return view('ideas.index', [
         'ideas' => $ideas
     ]);
 
 });
 
+// show
+Route::get('/ideas/{idea}', function (Idea $idea) {
+    // $idea= Idea::findOrFail($id);
+
+    // $idea = Idea::find($id);
+    // if (is_null($idea)) {
+    //     abort(404);
+    // }
+
+    return view('ideas.show', [
+        'idea' => $idea
+    ]);
+});
+
+// edit
+Route::get('/ideas/{idea}/edit', function (Idea $idea) {
+    return view('ideas.edit', [
+        'idea'=> $idea
+    ]);
+});
+
+// update
+Route::patch('/ideas/{idea}', function (Idea $idea) {
+    $idea->update([
+        'description' => request('description'),
+    ]);
+
+    return redirect("/ideas/{$idea->id}");
+});
+
+// store
 Route::post('/ideas', function () {
 
     $idea = Idea::create([
@@ -23,11 +51,13 @@ Route::post('/ideas', function () {
         'state' => 'pending'
     ]);
 
-    return redirect('/');
+    return redirect('/ideas');
 });
 
-Route::get('/delete-ideas', function () {
-    session()->forget('ideas');
 
-    return redirect('/');
+// destroy
+Route::delete('/ideas/{idea}', function (Idea $idea) {
+    $idea->delete();
+
+    return redirect('/ideas');
 });
